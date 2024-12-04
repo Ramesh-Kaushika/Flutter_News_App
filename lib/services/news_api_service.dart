6531ebbf9a486a1d2162880baf5f938559dcb085
Ramesh Kaushika De Silva <ramesh.kaushika2245@gmail.com>
2024-12-04 23:55:59 +0530

    pending service class

--- lib/services/news_api_service.dart
+++ lib/services/news_api_service.dart
@@ -1,8 +1,8 @@
-
+import 'package:news_app/models/article.dart';
 
 class NewsApiService {
-
   static const String _baseUrl = 'https://newsapi.org/v2';
   static const String _apiKey = 'af1ec12c17314a37afe9569ec7640541';
-  
+
+  Future<List<Article>> fetchTopHeadlines() async {}
 }
