832bd20d10285cc8dca0ab95f5abc3aee90d482c
Ramesh Kaushika De Silva <ramesh.kaushika2245@gmail.com>
2024-12-04 23:58:42 +0530

    import service class in provider class

--- lib/view_models/news_provider.dart
+++ lib/view_models/news_provider.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:news_app/services/news_api_service.dart';
 
 class NewsProvider with ChangeNotifier {
-  final NewsApiService _newsApiService = News
+  final NewsApiService _newsApiService = NewsApiService();
 }
